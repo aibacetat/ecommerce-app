eedc85261b09bbdd873a10469854f6f9cd41f836
aibacetat <aaron.ibaceta@hotmail.com>
2023-04-01 16:35:31 -0300

    fix router

--- emart/src/App.js
+++ emart/src/App.js
@@ -1,21 +1,19 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Products from "./components/Products";
 import Product from "./components/Product";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path="products/*" element={<Products />} />
         <Route path="products/:id" element={<Product />} />
       </Routes>
-      </BrowserRouter>
     </>
   );
 }
