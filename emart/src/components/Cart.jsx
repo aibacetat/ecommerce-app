c61beffe3bb6ae69c94ad130cfcab8a4107c2625
aibacetat <aaron.ibaceta@hotmail.com>
2023-04-01 20:09:27 -0300

    update cart 1

--- emart/src/components/Cart.jsx
+++ emart/src/components/Cart.jsx
@@ -1,39 +1,40 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { delCart } from "../redux/action";
-import Product from "./Product";
+import { useDispatch, useSelector } from "react-redux";
+import handleCart from "../redux/reducer/handleCart";
 
 const Cart = () => {
-    const state = useSelector((state)=> state.addCart)
+    const state = useSelector((state)=> state.handleCart)
     const dispatch = useDispatch()
 
-    const cartItems = (Product) => {
-        return(
-            <div className="px-4 my-5 bg-light rounded-3" key={Product.id}>
-                <div className="container py-4">
-                    <button className="btn-close float-end" aria-label="Close"></button>
-                    <div className="row justify-content-center">
-                        <div className="col-md-4">
-                            <img src={Product.image} alt={Product.title}></img>
-                        </div>
-                        <div className="col-md-4">
 
-                       
-                            <h3></h3>
-                            <p></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    } 
-
-    return  (
-        <>
-            {state.lengh !==0 && state.map(cartItems)}
-        </>
-    );
+const cartItems = (handleCart) => {
+return(
+<div className="px-4 my-5 bg-light rounded-3">
+    <div className="container py-4">
+<div className="row">
+    <div className="col-md-4">
+        <img src={Product.image} alt={product.title} height="200px" width="180px" />
+    </div>
+    <div className="col-md-4">
+        <h3>{product.title}</h3>
+        <p className="lead fw-bold">
+            {product.qty} X ${product.price} = $
+            {product.qty * product.price}
+        </p>
+        <button className="btn btn-outline-dark me-4"
+        onClick={()=>handleButton(product)}>
+            <i className="fa fa-minus"></i>
+        </button>
+        <button className="btn btn-outline-dark"
+        onClick={()=> handleButton(product)}>
+            <i className="fa fa-plus"></i>
+        </button>
+    </div>
+</div>
+</div>
+</div>
+);
+}
 }
 
 export default Cart;
