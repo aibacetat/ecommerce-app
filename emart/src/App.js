11e8f128863078664b2f74a90b6ade1e94bf7089
aibacetat <aaron.ibaceta@hotmail.com>
2023-04-01 14:55:41 -0300

    react router v6

--- emart/src/App.js
+++ emart/src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from "./components/Products";
 
 function App() {
   return (
-<>
-    < NavBar />
-    <Home />
-</>
+    <>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="products/*" element={<Products />} />
+      </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- emart/src/components/NavBar.jsx
+++ emart/src/components/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
         <div className="container">
-          <a className="navbar-brand fw-bold fs-4" href="#">
+          <Link className="navbar-brand fw-bold fs-4" to="/">
             ReactTech Shop
              <i className="fa fa-camera-retro" aria-hidden="true"></i>
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -23,33 +24,33 @@ const NavBar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <Link className="nav-link" to="/products">
                   Productos
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <Link className="nav-link" to="/about">
                   Acerca
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <Link className="nav-link" to="/contact">
                   Contacto
-                </a>
+                </Link>
               </li>
             </ul>
             <div className="buttons">
-                <a href="" className="btn btn-outline-dark">
-                    <i className="fa fa-sign-in me-1"></i>Ingresar</a>
-                    <a href="" className="btn btn-outline-dark ms-2">
-                    <i className="fa fa-user-plus me-1"></i>Registrarse</a>
-                    <a href="" className="btn btn-outline-dark ms-2">
-                    <i className="fa fa-shopping-cart me-1"></i>Carrito (0)</a>
+                <Link to="/login" className="btn btn-outline-dark">
+                    <i className="fa fa-sign-in me-1"></i>Ingresar</Link>
+                    <Link to="/register" className="btn btn-outline-dark ms-2">
+                    <i className="fa fa-user-plus me-1"></i>Registrarse</Link>
+                    <Link to="/cart" className="btn btn-outline-dark ms-2">
+                    <i className="fa fa-shopping-cart me-1"></i>Carrito (0)</Link>
             </div>
           </div>
         </div>
